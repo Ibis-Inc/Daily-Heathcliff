3e5012de4b726cbbf52654520257274ec4bba9cd
wiste <memeboibeep@outlook.com>
2025-10-11 01:01:43 +0100

    removed unnecessqry libraries from imports

--- main.py
+++ main.py
@@ -10,11 +10,8 @@ from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 from webdriver_manager.firefox import GeckoDriverManager
 from lxml import html
-import schedule
-import time
 import asyncio
 import timedelta
-import pause
 
 
 load_dotenv()
